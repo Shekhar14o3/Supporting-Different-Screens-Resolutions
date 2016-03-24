ab008175ef31d9f406ae4ca2a1abeb2f2fe238cf
Shekhar14o3 <aman.shekhar4@gmail.com>
2016-03-24 12:04:59 +0530

    Initial commit

---
+++ app/src/main/java/post/technoblog/differentdevices/DifferentDevices.java
@@ -0,0 +1,20 @@
+package post.technoblog.differentdevices;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+// Check out the different layouts for every screens.
+
+public class DifferentDevices extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_different_devices);
+    }
+}
